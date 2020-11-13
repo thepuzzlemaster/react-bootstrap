82c13c91e2675acfd04d02c2f883013ffc633acb
kyletsang <6854874+kyletsang@users.noreply.github.com>
2020-11-13 10:23:35 -0800

    Revert the custom toggle examples to be card-based

--- www/src/examples/Accordion/ContextAwareToggle.js
+++ www/src/examples/Accordion/ContextAwareToggle.js
@@ -22,42 +22,22 @@ function ContextAwareToggle({ children, eventKey, callback }) {
 function Example() {
   return (
     <Accordion defaultActiveKey="0">
-      <Accordion.Item>
-        <Accordion.Header>
-          <ContextAwareToggle eventKey="0">
-            Accordion Item #1
-          </ContextAwareToggle>
-        </Accordion.Header>
+      <Card>
+        <Card.Header>
+          <ContextAwareToggle eventKey="0">Click me!</ContextAwareToggle>
+        </Card.Header>
         <Accordion.Collapse eventKey="0">
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
+          <Card.Body>Hello! I'm the body</Card.Body>
         </Accordion.Collapse>
-      </Accordion.Item>
-      <Accordion.Item>
-        <Accordion.Header>
-          <ContextAwareToggle eventKey="1">
-            Accordion Item #2
-          </ContextAwareToggle>
-        </Accordion.Header>
+      </Card>
+      <Card>
+        <Card.Header>
+          <ContextAwareToggle eventKey="1">Click me!</ContextAwareToggle>
+        </Card.Header>
         <Accordion.Collapse eventKey="1">
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
+          <Card.Body>Hello! I'm another body</Card.Body>
         </Accordion.Collapse>
-      </Accordion.Item>
+      </Card>
     </Accordion>
   );
 }
--- www/src/examples/Accordion/CustomToggle.js
+++ www/src/examples/Accordion/CustomToggle.js
@@ -17,38 +17,22 @@ function CustomToggle({ children, eventKey }) {
 function Example() {
   return (
     <Accordion defaultActiveKey="0">
-      <Accordion.Item>
-        <Accordion.Header>
-          <CustomToggle eventKey="0">Accordion Item #1</CustomToggle>
-        </Accordion.Header>
+      <Card>
+        <Card.Header>
+          <CustomToggle eventKey="0">Click me!</CustomToggle>
+        </Card.Header>
         <Accordion.Collapse eventKey="0">
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
+          <Card.Body>Hello! I'm the body</Card.Body>
         </Accordion.Collapse>
-      </Accordion.Item>
-      <Accordion.Item>
-        <Accordion.Header>
-          <CustomToggle eventKey="1">Accordion Item #2</CustomToggle>
-        </Accordion.Header>
+      </Card>
+      <Card>
+        <Card.Header>
+          <CustomToggle eventKey="1">Click me!</CustomToggle>
+        </Card.Header>
         <Accordion.Collapse eventKey="1">
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
+          <Card.Body>Hello! I'm another body</Card.Body>
         </Accordion.Collapse>
-      </Accordion.Item>
+      </Card>
     </Accordion>
   );
 }
